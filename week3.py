3fe3b64b16c68ac34b4aa2247057a9deb3d1141c
Hendi Lie <lie.hendi@gmail.com>
2017-08-14 21:37:46 +1000

    Updated week 3 tutorial + scripts
    Updated week 3 tutorial + scripts

--- week3.py
+++ week3.py
@@ -9,8 +9,30 @@ import pydot
 from io import StringIO
 from sklearn.tree import export_graphviz
 
-# preprocessing step
+# helper functions from the end. Put these two in dm_tools at the end of tutorial. Import it with this method instead
+# from dm_tools import analyse_feature_importance, visualize_decision_tree
 
+def analyse_feature_importance(dm_model, feature_names, n_to_display=20):
+    # grab feature importances from the model
+    importances = dm_model.feature_importances_
+    
+    # sort them out in descending order
+    indices = np.argsort(importances)
+    indices = np.flip(indices, axis=0)
+
+    # limit to 20 features, you can leave this out to print out everything
+    indices = indices[:n_to_display]
+
+    for i in indices:
+        print(feature_names[i], ':', importances[i])
+
+def visualize_decision_tree(dm_model, feature_names, save_name):
+    dotfile = StringIO()
+    export_graphviz(dm_model, out_file=dotfile, feature_names=feature_names)
+    graph = pydot.graph_from_dot_data(dotfile.getvalue())
+    graph[0].write_png(save_name) # saved in the following file
+
+# preprocessing step
 df = data_prep()
 
 # train test split
@@ -27,52 +49,47 @@ y_pred = model.predict(X_test)
 print(classification_report(y_test, y_pred))
 print(accuracy_score(y_test, y_pred))
 
-# grid search CV
-params = {'criterion': ['gini', 'entropy'], 'max_depth': range(3, 10), 'min_samples_leaf': range(20, 200, 20)}
+##########################################
+# grid search CV #1
+params = {'criterion': ['gini', 'entropy'],
+          'max_depth': range(3, 10),
+          'min_samples_leaf': range(20, 60, 10)}
 
-cv = GridSearchCV(param_grid=params, estimator=DecisionTreeClassifier(), cv=5)
+cv = GridSearchCV(param_grid=params, estimator=DecisionTreeClassifier(), cv=10)
 cv.fit(X_train, y_train)
+
+print("Train accuracy:", cv.score(X_train, y_train))
+print("Test accuracy:", cv.score(X_test, y_test))
+
+# test the best model
 y_pred = cv.predict(X_test)
 print(classification_report(y_test, y_pred))
-print(accuracy_score(y_test, y_pred))
 
+# print parameters of the best model
 print(cv.best_params_)
 
+##########################################
+
 # grid search CV #2
-params = {'criterion': ['gini', 'entropy'], 'max_depth': range(2, 5), 'min_samples_leaf': range(10, 30, 10)}
+params = {'criterion': ['gini'],
+          'max_depth': range(2, 5),
+          'min_samples_leaf': range(40, 61, 5)}
+
+cv = GridSearchCV(param_grid=params, estimator=DecisionTreeClassifier(), cv=10)
 cv.fit(X_train, y_train)
-cv = GridSearchCV(param_grid=params, estimator=DecisionTreeClassifier(), cv=5)
-cv.fit(X_train, y_train)
+
+print("Train accuracy:", cv.score(X_train, y_train))
+print("Test accuracy:", cv.score(X_test, y_test))
+
+# test the best model
 y_pred = cv.predict(X_test)
 print(classification_report(y_test, y_pred))
-print(accuracy_score(y_test, y_pred))
 
-# grid search on max leaf nodes only
-params = {'max_leaf_nodes': range(2, 15)}
-cv = GridSearchCV(param_grid=params, estimator=DecisionTreeClassifier(), cv=5)
-cv.fit(X_train, y_train)
-y_pred = cv.predict(X_test)
-print(accuracy_score(y_test, y_pred))
-print(classification_report(y_test, y_pred))
+# print parameters of the best model
+print(cv.best_params_)
 
-# feature importances, retrain the CV first
-params = {'criterion': ['gini', 'entropy'], 'max_depth': range(2, 5), 'min_samples_leaf': range(10, 30, 10)}
-cv = GridSearchCV(param_grid=params, estimator=DecisionTreeClassifier(), cv=5)
-cv.fit(X_train, y_train)
+##########################################
 
-importances = cv.best_estimator_.feature_importances_
-names = X.columns
-
-indices = np.argsort(importances)
-indices = np.flip(indices, axis=0)
-
-for i in indices:
-    print(names[i], ':', importances[i])
-
-
-# visualize
-model = cv.best_estimator_
-dotfile = StringIO()
-export_graphviz(model, out_file=dotfile, feature_names=names)
-graph = pydot.graph_from_dot_data(dotfile.getvalue())
-graph[0].write_png("week3_dt_viz.png")
+# feature importances on best CV model
+analyse_feature_importance(cv.best_estimator_, X.columns, 20)
+visualize_decision_tree(cv.best_estimator_, X.columns, "dm_best_cv.png")
